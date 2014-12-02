523ed9db96848bd8002ca07919d9173e63e1a518
Johannes Olsson <johanneo@csc.kth.se>
2014-12-02 16:59:46 +0100

    stuck on print from both processes

--- pipes/src/digenv.c
+++ pipes/src/digenv.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
-int main(int argc, char c)
+
+#define READ 0
+#define WRITE 1
+#define BSIZE 1000
+
+int main(int argc, char **argv)
 {
-	int *r, *w;
-	int *fd[2] = {r, w};
+	printf("parent process reporting for duty\n");
+	
+	char buf[BSIZE];
+	
+	int fd[2];
 	pipe(fd);
+	
 
-	pid_t pid, c;
+	int c = fork();
 
-	c = fork();
+	switch(c)
+	{
+		case 0: /*child*/
+			//close(READ);
+			//dup2(fd[WRITE], 1);
+			//dup2(fd[WRITE], 2);
+			//execlp("bin/cat", "hej", " man", NULL);
+			//write(fd[WRITE], "hellooo", BSIZE);
+			break;
+		default: /*parent*/
+			printf("so this happens...\n");
+			//read(fd[READ], buf, BSIZE);
+			//printf("%s\n", buf);
+			close(READ);
+			close(WRITE);
+			break;
+	}
 
-	pid = getpid();
+	return 0;
 }
