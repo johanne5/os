0e342158c04a5ea2f75892f587af2455bc093d65
Johannes Olsson <johanneo@csc.kth.se>
2014-12-03 19:24:46 +0100

    pipes-lab klar, inkl kommentarer

--- pipes/src/digenv.c
+++ pipes/src/digenv.c
@@ -3,68 +3,61 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+							//macros for readability
+#define READ fd[0]			//macro for file descriptor's read end
+#define WRITE fd[1]			//macro for file descriptor's write end
 
-#define READ fd[0]
-#define WRITE fd[1]
-#define BSIZE 100000
+/*
+If this program is called with no arguments, it will execute sort on the output from printenv and forward to a pager.
+If the PAGER environment variable is set, the program will use that. Otherwise less. If less fails, the program will use more.
+If this program is called with one or more arguments, they will all be passed to grep which is executed before sort in the above sequence when program is called with no arguments.
+*/
 
+//argc: the number of arguments in the program call
+//argv: a string array with parameters
 int main(int argc, char **argv)
 {
+	argv[0] = "grep";							//changing the first argument which is something like ./digenv to grep in preparation for call to grep
+	const char **grep = (const char**) argv;	//string array for call to grep
+
+	const char *pager[] = {NULL!=getenv("PAGER") ? getenv("PAGER") : "less", NULL};	//string array for call to the pager (user defined or less)	
+
+	const char *printenv[] = {"printenv", NULL}; //string array for call to printenv
+	const char *sort[] = {"sort", NULL};	//string array for call to sort
+
+	const char **commands[] = {printenv, grep, sort, NULL};	//list of programs to run before print-out to pager
+	const char ***cmd = commands;	//pointer used to traverse the list of programs
+
+	int fd[2], readbackup;	//two integers for the filedescriptors read and write ends respectively, one to pass read end between pipes
 	
-	char grepargs[1000];
-
-	int i=0;
-	argc--;
-	while(0!=argc--){
-		
-		while('\0'!=argv[1][0])
-		{
-			grepargs[i++] = argv[1][0];
-			argv[1]++;
-		}
-		if(0!=argc)
-		{
-			grepargs[i++] = '\\';
-			grepargs[i++] = '|';
-		}
-		
-		argv++;
-	}
-
-	const char *printenv[] = {"printenv", NULL};
-	const char *sort[] = {"sort", NULL};
-	const char *grep[] = {"grep", grepargs, NULL};
-	const char *less[] = {"less", "-", NULL};
-	const char *echo[] = {"echo", "hej\napa\n", NULL};
-
-	const char **commands[] = {printenv, grep, sort, NULL};
-	const char ***cmd = commands;
-	char buf[BSIZE];
-	
-	int fd[2], readbackup;
-	
-
-	while(NULL != *cmd)
+	/* Loop to iterate over the programs to call. The list is null-terminated */
+	while(NULL != cmd[0])
 	{
-		pipe(fd);
-		switch(fork())
+		if(1==argc && "grep"==cmd[0][0])	//if program for this iteration is grep and no arguments have been passed to this program, grep is not run
 		{
-			case 0:
-				
-				dup2(readbackup, 0);
-				dup2(WRITE, 1);
-				close(WRITE);
-				close(READ);
-				execvp(cmd[0][0],(char *const*) cmd[0]);
-			default:
-				wait(NULL);
-				readbackup = READ;
-				close(WRITE);
-				cmd = cmd + 1;			
+			cmd++;
+			continue;
 		}
 
+		pipe(fd);	//creating new pipe with our file descriptors
+		switch(fork())	//forking this process
+		{
+			case 0:		//only the child process runs this case
+				dup2(readbackup, 0);	//copying our saved file descriptor's read end to standard input
+				dup2(WRITE, 1);	//copying our file descriptor's write end to standard output
+				close(WRITE);	//closing our channel to the file descriptor's write end
+				close(READ);	//closing our channel to the file descriptor's read end
+				execvp(cmd[0][0],(char *const*) cmd[0]); //executing a command from the command's list
+			default:	//only the parent process runs this case
+				wait(NULL);	//waiting for the child process to terminate
+				readbackup = READ; //backing up the read end of our file descriptor
+				close(WRITE); //closing our channel to the file descriptor's write end
+				cmd++; //incrementing the pointer to the command's list
+		}
 	}
-	close(WRITE);
-	dup2(READ, 0);
-	execvp(less[0], (char *const*) less);
+	dup2(readbackup, 0);	//copying our saved file descriptor's read end to standard input
+	close(READ);	//closing our channel to the file descriptor's read end
+	execvp(pager[0], (char *const*) pager);	//executing call to the pager
+	pager[0]="more"; //if user defined pager was not defined or less did not work, change pager to more
+	execvp(pager[0], (char *const*) pager); //retry executing call to the pager
 }
